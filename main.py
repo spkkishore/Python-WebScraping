9982f7304cf8812c956076e31fa05da1cc217108
S P Krishna Kishore <52457333+spkkishore@users.noreply.github.com>
2022-05-17 17:41:37 +0000

    updated to get time value

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ def get_driver():
   driver.get("http://automated.pythonanywhere.com/login/")
   return driver
 
+def clean_text(text):
+  """Extract only the temperature from text"""
+  output = float(text.split(": ")[1])
+  return output
+  
 def main():
   driver = get_driver()
   driver.find_element(by="id", value="id_username").send_keys("automated")
@@ -21,6 +26,8 @@ def main():
   driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
   time.sleep(2)
   driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
-  print(driver.current_url)
+  time.sleep(2)
+  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]").text
+  return clean_text(element)
 
 print(main())
